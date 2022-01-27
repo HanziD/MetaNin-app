6ac4462a25977321aa6a9e3c7a5d647f7a7b5687
HanziD <jbz0001@mix.wvu.edu>
2022-01-26 20:06:31 -0500

    Lab 1: Footer created

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'fm-footer',
+    templateUrl: 'footer.component.html',
+    styleUrls: ['footer.component.css']
+
+})
+
+export class footer{
+
+}
