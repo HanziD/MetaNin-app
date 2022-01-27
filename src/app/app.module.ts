27f8c0d89c2fd56c6b7fbb972e95d5639d85466a
HanziD <jbz0001@mix.wvu.edu>
2022-01-26 20:06:08 -0500

    Initialized footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormControlDirective } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { footer } from './footer/footer.component';
 import { header } from './header/topnavbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    header
+    header,
+    footer
   ],
   imports: [
     BrowserModule
